5de3f20b6cdbc57dba5dd002dac75375e20ed9db
Krixsh <krishnendughosal999@gmail.com>
2024-02-18 14:46:53 +0530

    server setup for graphQl

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import cors
+
+const app = express();
+
+
+// Allow all origins for demonstration purposes
+app.use(cors());
+
+mongoose.connect('mongodb+srv://krishnendughosal999:Hardwork%4034@cluster0.ircni7a.mongodb.net/');
+mongoose.connection.once('open', () => {
+    console.log('Connected to Krish-personal-Database');
+})
+
+
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({  //default middileware
+    schema,            // schema to interact
+    graphiql:true  //a tool to that route
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
---
+++ models/author.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const authorSchema = new Schema({
+    name: String,
+    age: Number,
+    authorId: String
+
+});
+
+module.exports = mongoose.model('Author', authorSchema);
